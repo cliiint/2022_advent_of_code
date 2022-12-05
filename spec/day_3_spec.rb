1b34adf729d11f8c21dec2977b5d9e9f996ea036
Clint Philbin <clint.philbin@gmail.com>
2022-12-04 18:22:44 -0800

    Fix day 3 test data

--- spec/day_3_spec.rb
+++ spec/day_3_spec.rb
@@ -1,7 +1,7 @@
 require_relative '../day_3/main'
 
 describe RuckSack do
-  rucksack = RuckSack.new('gnJudqwy')
+  rucksack = RuckSack.new('gnJudgwy')
 
   it 'should have expected read properties' do
     expect(rucksack.instance_variable_defined?(:@compartment_1)).to eq(true)
@@ -11,7 +11,7 @@ describe RuckSack do
     expect(rucksack.compartment_1.length).to eq(rucksack.compartment_2.length)
   end
 
-  it 'should have a shared item' do
-    expect(rucksack.shared_item).to_not be_nil
+  it 'should have the expected shared item' do
+    expect(rucksack.find_shared_item).to eq('g')
   end
 end
