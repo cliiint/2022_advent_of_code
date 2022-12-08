ac1c34f69a989ae8612953ccfdebdb87d8f5e170
Clint Philbin <clint.philbin@gmail.com>
2022-12-07 16:05:34 -0800

    Day 4 DRY it up

--- day_4/main.rb
+++ day_4/main.rb
@@ -1,4 +1,8 @@
 class Solution
+  def initialize
+    @range_pairs = init_ranges
+  end
+
   def self.create_ranges(str)
     split = str.split(',')
     a = split[0].split('-')
@@ -7,41 +11,38 @@ class Solution
     [a[0].to_i..a[1].to_i, b[0].to_i..b[1].to_i]
   end
 
+  def self.contain?(range_1, range_2)
+    (range_1.first >= range_2.first && range_1.last <= range_2.last) ||
+      (range_2.first >= range_1.first && range_2.last <= range_1.last)
+  end
+
+  def self.overlap?(range_1, range_2)
+    (range_1.include?(range_2.first) || range_1.include?(range_2.last)) ||
+      (range_2.include?(range_1.first) || range_2.include?(range_1.last))
+  end
+
   def part_1
     contained_assignments = 0
-
-    lines = File.readlines(__dir__ + '/data.txt', chomp: true)
-    lines.each do |l|
-      ranges = Solution.create_ranges(l)
-      a, b = ranges[0], ranges[1]
-      overlap =
-        (a.first >= b.first && a.last <= b.last) ||
-          (b.first >= a.first && b.last <= a.last)
-
-      contained_assignments += 1 if overlap
-    end
-
+    @range_pairs.each { | r | contained_assignments += 1 if Solution.contain?(r[0], r[1] ) }
     contained_assignments
     # 599
   end
 
   def part_2
     contained_assignments = 0
-
-    lines = File.readlines(__dir__ + '/data.txt', chomp: true)
-    lines.each do |l|
-      ranges = Solution.create_ranges(l)
-      a, b = ranges[0], ranges[1]
-      overlap =
-        (a.include?(b.first) || a.include?(b.last)) ||
-          (b.include?(a.first) || b.include?(a.last))
-
-      contained_assignments += 1 if overlap
-    end
-
+    @range_pairs.each { | r | contained_assignments += 1 if Solution.overlap?(r[0], r[1] ) }
     contained_assignments
     # 928
   end
+
+  private
+
+    def init_ranges
+      lines = File.readlines(__dir__ + '/data.txt', chomp: true)
+      ranges = []
+      lines.each { |l| ranges << Solution.create_ranges(l) }
+      @range_pairs = ranges
+    end
 end
 
 solution = Solution.new
